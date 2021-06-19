d657f539b6cf321d4e76a6ce0d4aa7542e253001
Cooper Jackson <davisjaxon@gmail.com>
2021-06-19 00:19:08 -0700

    heroku push

---
+++ data/migrations/20210619060916_first-migration.js
@@ -0,0 +1,48 @@
+
+exports.up = function(knex) {
+    return knex.schema
+
+    .createTable('users', users => {
+        users.increments();
+        users.string('username', 255).notNullable().unique();
+        users.string('password', 255).notNullable();
+    })
+    .createTable('boards', table => {
+        table.increments('board_id');
+        table.text('username', 255).notNullable().unique();
+        table.text('password', 255).notNullable();
+    })
+    .createTable('articles', table => {
+        table.increments('article_id')
+        table.text('title', 255).notNullable()
+        table.text('url').notNullable()
+        table.text('category', 128)
+        table.integer('rank')
+        table.integer('board_id')
+            .unsigned()
+            .notNullable()
+            .references('board_id')
+            .inTable('boards')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE')
+    })
+    .createTable('categories', table => {
+        table.increments('category_id')
+        table.text('name', 255).notNullable()
+        table.integer('article_id')
+            .unsigned()
+            .notNullable()
+            .references('article_id')
+            .inTable('articles')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE')
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema
+    .dropTableIfExists('categories')
+    .dropTableIfExists('articles')
+    .dropTableIfExists('boards')
+    .dropTableIfExists('users')
+};
